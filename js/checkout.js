1b31ff58d6aba2e222e8fea0128d58f9514046e4
Iselinklementin <69785627+Iselinklementin@users.noreply.github.com>
2021-03-14 22:30:42 +0100

    edit checkout

--- js/checkout.js
+++ js/checkout.js
@@ -17,15 +17,24 @@
 
 // const test = document.querySelector("h1")
 
-const button = document.querySelector("button")
-const email = document.querySelector("#email");
 const form = document.querySelectorAll("form");
+const button = document.querySelector("button");
 
+
+const email = document.querySelector("#email");
+const street = document.querySelector("#street");
+const state = document.querySelector("#state");
+const postal = document.querySelector("#post-code")
 const fullName = document.querySelector(".fullname");
 const nameInput = document.querySelector("#name");
 
-const nameError = document.querySelector("#nameError");
-const emailError = document.querySelector("#emailError")
+const nameError = document.querySelector("#name-error");
+const emailError = document.querySelector("#email-error");
+const streetError = document.querySelector("#street-error");
+const postalError = document.querySelector("#postal-error");
+const stateError = document.querySelector("#state-error");
+
+const radioPayment = document.querySelectorAll(".radio-pay")
 
 function checkButton() {
 console.log("works")
@@ -36,7 +45,7 @@ function validate() {
 
     // event.preventDefault();
       
-    if(required(fullName.value)) {
+    if (required(fullName.value)) {
         nameInput.style.borderColor = "red";
         nameError.style.display = "block";
         // nameLabel.after(fail);
@@ -47,17 +56,59 @@ function validate() {
         // nameLabel.appendChild(check);
     }
 
-    if(validateEmail(email.value)) {
+    if (validateEmail(email.value)) {
         email.style.borderColor = "rgb(42, 179, 42)";
         emailError.style.display = "none";
         // emailLabel.after(check);
     } else {
         email.style.borderColor = "red";
         emailError.style.display = "block";
-        // emailLabel.appendChild(fail);
+        // email.appendChild(fail);
     }
     
+    if (required(street.value)) {
+        street.style.borderColor = "red";
+        streetError.style.display = "block";
+    } else {
+        street.style.borderColor = "rgb(42, 179, 42)";
+        streetError.style.display = "none";
+    }
+
+    if (required(state.value)) {
+        state.style.borderColor = "red";
+        stateError.style.display = "block";
+    } else {
+        state.style.borderColor = "rgb(42, 179, 42)";
+        stateError.style.display = "none";
+    }
+
+    if (required(postal.value)) {
+        postal.style.borderColor = "red";
+        postalError.style.display = "block";
+    } else {
+        postal.style.borderColor = "rgb(42, 179, 42)";
+        postalError.style.display = "none";
+    }
 }
+
+function checkRadioPayment() {
+
+const checkPayment = document.querySelectorAll("input[name="payment"]:checked");
+
+    for (let i = 0; i < checkPayment.length; i++) {
+
+        let radioPay = checkPayment[i];
+
+        console.log(radioPay)
+
+        if(radioPay.checked) {
+            console.log("radio worked")
+            console.log(radioPay.value)
+        }
+    }
+}
+
+checkRadioPayment();
 
 function required(input) {
     if (input.trim().length === 0) {
@@ -69,8 +120,13 @@ function required(input) {
 
 validate();
 
+radioPayment.addEventListener("keyup", validate);
 fullName.addEventListener("keyup", validate);
 email.addEventListener("keyup", validate);
+street.addEventListener("keyup", validate);
+postal.addEventListener("keyup", validate);
+state.addEventListener("keyup", validate);
+
 
 function validateEmail(email) {
     const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
