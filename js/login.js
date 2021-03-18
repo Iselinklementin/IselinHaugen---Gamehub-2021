a00c7d42bb4a90db763797c957343373ab49763d
Iselinklementin <69785627+Iselinklementin@users.noreply.github.com>
2021-03-18 13:06:31 +0100

    login

--- js/login.js
+++ js/login.js
@@ -6,15 +6,9 @@ const password = document.querySelector("#password");
 
 const pswError = document.querySelector("#password-error");
 const emailError = document.querySelector("#email-error");
-const buttonError = document.querySelector(".error-button")
+const buttonError = document.querySelector(".error-button");
+const inputs = document.querySelectorAll("input");
 
-/* function checkLength(value, len) {
-    if (value.trim().length > len) {
-    return true;
-} else {
-    return false;
-}
-}; */
 
  function validateEmail(email) {
     const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -27,55 +21,15 @@ const isPasswordSecure = (password) => {
     return re.test(password);
 };
 
-/* function validateLogin() {
-    if (validateEmail(email.value)) {
-        email.style.borderColor = "rgb(42, 179, 42)";
-        emailError.style.display = "none";
-        return true;
-    } else {
-        email.style.borderColor = "red";
-        emailError.style.display = "block";
-    }
-}
-
-function validatePassword() {
-    if (isPasswordSecure(password.value)) {
-        password.style.borderColor = "rgb(42, 179, 42)";
-        pswError.style.display = "none";
-    } else {
-        password.style.borderColor = "red";
-        pswError.style.display = "block";
-    }
-}
-
-function checkValidation () {
-    if (email.addEventListener("input", validateLogin)) {
-        button.disabled = false;
-        buttonError.style.display = "none";
-    } else {
-        button.disabled = true;
-        buttonError.style.display = "block";
-    }
-}
- */
-/* if (!validateLogin) {
-    button.disabled = false;
-    buttonError.style.display = "none";
-} else {
-    button.disabled = true;
-    buttonError.style.display = "block";
-}
- */
-
-/* else if (!validateLogin() && !validatePassword) {
-    button.disabled = true;
-    buttonError.style.display = "block";
-    } */
-
 email.addEventListener("input", function() {
     if (validateEmail(email.value)) {
         email.style.borderColor = "rgb(42, 179, 42)";
         emailError.style.display = "none";
+
+        if (buttonError.style.display = "block") {
+            buttonError.style.display = "none"
+        } 
+
     } else {
         email.style.borderColor = "red";
         emailError.style.display = "block";
@@ -87,42 +41,32 @@ password.addEventListener("keyup", function() {
     if (isPasswordSecure(password.value)) {
         password.style.borderColor = "rgb(42, 179, 42)";
         pswError.style.display = "none";
+
+        
+        if (buttonError.style.display = "block") {
+            buttonError.style.display = "none"
+        }
     } else {
         password.style.borderColor = "red";
         pswError.style.display = "block";
     }
 });
 
-button.addEventListener("click", function submitForm(event) {
+function submitForm(event) {
     event.preventDefault();
 
-   let mail = validateEmail(email.value);
-   let psw = isPasswordSecure(password.value);
-
-   if (mail && psw) {
-    buttonError.style.display = "none";
-   } else {
-    button.disabled = true;
-    buttonError.style.display = "block";
-   }
-/* 
-    if (validateEmail(email.value) && isPasswordSecure(password.value)) {
-        buttonError.style.display = "none";
+    let mail = validateEmail(email.value);
+    let psw = isPasswordSecure(password.value);
+ 
+    if (mail && psw) {
+    button.disabled = false;
+     buttonError.style.display = "none";
+     document.location.href = "account.html";
     } else {
-        button.disabled = true;
-        buttonError.style.display = "block";
+     button.disabled = true;
+     buttonError.style.display = "block";
+ }
+     form.reset();
+ };
 
-        if (validateEmail(email.value.onchange) || isPasswordSecure(password.value.onchange)) {
-            buttonError.style.display = "none";
-        }
-    } */
-
-    console.log("happend")
-    form.reset();
-}
-);
-
-
-if (button.disabled) {
-    button.style.backgroundColor = "green"
-}
+ button.addEventListener("click", submitForm);
