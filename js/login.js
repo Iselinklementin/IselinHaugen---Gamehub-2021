88a0edfc17e5b18dfd85fd703224f53de10af849
Iselinklementin <69785627+Iselinklementin@users.noreply.github.com>
2021-03-18 11:55:21 +0100

    login form

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 const form = document.querySelector("form");
-const button = document.querySelector(".button");
+const button = document.querySelector("#submitBtn");
 
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
@@ -8,41 +8,13 @@ const pswError = document.querySelector("#password-error");
 const emailError = document.querySelector("#email-error");
 const buttonError = document.querySelector(".error-button")
 
-function validate() {
-
-    if (validateEmail(email.value)) {
-        email.style.borderColor = "rgb(42, 179, 42)";
-        emailError.style.display = "none";
-    } else {
-        email.style.borderColor = "red";
-        emailError.style.display = "block";
-    }
-
-    if (checkLength(password.value, 7)) {
-        password.style.borderColor = "rgb(42, 179, 42)";
-        pswError.style.display = "none";
-    } else {
-        password.style.borderColor = "red";
-        pswError.style.display = "block";
-    }
-    /* reqPassword(input); */
-
-    if (checkLength(password.value, 7) && validateEmail(email.value)) {
-        button.disabled = false;
-
-    } else {
-        button.disabled = true;
-        buttonError.style.display = "block";
-    }
-}
-
-function checkLength(value, len) {
+/* function checkLength(value, len) {
     if (value.trim().length > len) {
     return true;
 } else {
     return false;
 }
-};
+}; */
 
  function validateEmail(email) {
     const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -50,15 +22,107 @@ function checkLength(value, len) {
     return patternMatches
 };
 
-validateEmail();
+const isPasswordSecure = (password) => {
+    const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.{8,})");
+    return re.test(password);
+};
 
-function submitForm(event) {
-    event.preventDefault();
-
-    button.disabled = true;
-    form.reset();
+/* function validateLogin() {
+    if (validateEmail(email.value)) {
+        email.style.borderColor = "rgb(42, 179, 42)";
+        emailError.style.display = "none";
+        return true;
+    } else {
+        email.style.borderColor = "red";
+        emailError.style.display = "block";
+    }
 }
 
-button.addEventListener("submit", submitForm);
-email.addEventListener("keyup", validate);
-password.addEventListener("keyup", validate);
+function validatePassword() {
+    if (isPasswordSecure(password.value)) {
+        password.style.borderColor = "rgb(42, 179, 42)";
+        pswError.style.display = "none";
+    } else {
+        password.style.borderColor = "red";
+        pswError.style.display = "block";
+    }
+}
+
+function checkValidation () {
+    if (email.addEventListener("input", validateLogin)) {
+        button.disabled = false;
+        buttonError.style.display = "none";
+    } else {
+        button.disabled = true;
+        buttonError.style.display = "block";
+    }
+}
+ */
+/* if (!validateLogin) {
+    button.disabled = false;
+    buttonError.style.display = "none";
+} else {
+    button.disabled = true;
+    buttonError.style.display = "block";
+}
+ */
+
+/* else if (!validateLogin() && !validatePassword) {
+    button.disabled = true;
+    buttonError.style.display = "block";
+    } */
+
+email.addEventListener("input", function() {
+    if (validateEmail(email.value)) {
+        email.style.borderColor = "rgb(42, 179, 42)";
+        emailError.style.display = "none";
+    } else {
+        email.style.borderColor = "red";
+        emailError.style.display = "block";
+    }
+});
+
+
+password.addEventListener("keyup", function() {
+    if (isPasswordSecure(password.value)) {
+        password.style.borderColor = "rgb(42, 179, 42)";
+        pswError.style.display = "none";
+    } else {
+        password.style.borderColor = "red";
+        pswError.style.display = "block";
+    }
+});
+
+button.addEventListener("click", function submitForm(event) {
+    event.preventDefault();
+
+   let mail = validateEmail(email.value);
+   let psw = isPasswordSecure(password.value);
+
+   if (mail && psw) {
+    buttonError.style.display = "none";
+   } else {
+    button.disabled = true;
+    buttonError.style.display = "block";
+   }
+/* 
+    if (validateEmail(email.value) && isPasswordSecure(password.value)) {
+        buttonError.style.display = "none";
+    } else {
+        button.disabled = true;
+        buttonError.style.display = "block";
+
+        if (validateEmail(email.value.onchange) || isPasswordSecure(password.value.onchange)) {
+            buttonError.style.display = "none";
+        }
+    } */
+
+    console.log("happend")
+    form.reset();
+}
+);
+
+
+if (button.disabled) {
+    button.style.backgroundColor = "green"
+}
